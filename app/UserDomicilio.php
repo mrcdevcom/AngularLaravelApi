ef4a3496cc6e53c930f783746a314139e4c36cdc
Marco Castillo <marco@deplooy.com>
2021-05-05 20:22:07 -0500

    init

---
+++ app/UserDomicilio.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserDomicilio extends Model
+{
+    //
+}
---
+++ database/migrations/2021_05_06_011725_create_user_domicilios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("numero_exterior");
+            $table->string("colonia");
+            $table->string("cp");
+            $table->string("ciudad");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilios');
+    }
+}
